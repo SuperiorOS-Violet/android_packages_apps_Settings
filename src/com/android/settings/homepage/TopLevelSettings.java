9c47b73e3e87594b3a869662720f85129a33de18
Maitreya29 <maitreyapatni30@gmail.com>
2022-01-03 18:56:34 +0530

    Settings: Redesign dashboard
    Change-Id: Ia0744721052ea0d5a8f97c9b3b5a52ab8151e739

--- src/com/android/settings/homepage/TopLevelSettings.java
+++ src/com/android/settings/homepage/TopLevelSettings.java
@@ -28,6 +28,7 @@ import androidx.fragment.app.Fragment;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 import androidx.preference.PreferenceScreen;
+import android.util.Log;
 
 import com.android.settings.R;
 import com.android.settings.Utils;
@@ -43,6 +44,31 @@ public class TopLevelSettings extends DashboardFragment implements
         PreferenceFragmentCompat.OnPreferenceStartFragmentCallback {
 
     private static final String TAG = "TopLevelSettings";
+
+    public static final String[] CHANGE_LAYOUT_AND_ORDER_KEYS = {
+        "dashboard_tile_pref_com.oneplus.extras.DeviceSettings",
+        "dashboard_tile_pref_org.lineageos.settings.device",
+        "dashboard_tile_pref_org.lineageos.settings",
+        "dashboard_tile_pref_org.omnirom.devices",
+        "dashboard_tile_pref_org.omnirom.device",
+        "dashboard_tile_pref_com.poco.parts",
+        "dashboard_tile_pref_com.xiaomi.parts",
+        "dashboard_tile_pref_com.asus.zenparts"
+    };
+
+    public static final String[] CHANGE_LAYOUT_KEYS = {
+        "top_level_google",
+        "dashboard_tile_pref_com.google.android.gms.app.settings.GoogleSettingsIALink",
+        "dashboard_tile_pref_com.google.android.apps.wellbeing.settings.TopLevelSettingsActivity",
+        "top_level_wellbeing",
+    };
+
+    public static final int[] LAYOUTS_N = {
+        R.layout.rdnt_card_google,
+        R.layout.rdnt_card_google,
+        R.layout.rdnt_card_wellbeing,
+        R.layout.rdnt_card_wellbeing
+    };
 
     public TopLevelSettings() {
         final Bundle args = new Bundle();
@@ -99,21 +125,17 @@ public class TopLevelSettings extends DashboardFragment implements
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         super.onCreatePreferences(savedInstanceState, rootKey);
-        final PreferenceScreen screen = getPreferenceScreen();
-        if (screen == null) {
-            return;
-        }
-        // Tint the homepage icons
-        final int tintColor = Utils.getHomepageIconColor(getContext());
-        final int count = screen.getPreferenceCount();
-        for (int i = 0; i < count; i++) {
-            final Preference preference = screen.getPreference(i);
-            if (preference == null) {
-                break;
+        for (int i = 0; i < CHANGE_LAYOUT_KEYS.length; i++) {
+            Preference preference = findPreference(CHANGE_LAYOUT_KEYS[i]);
+            if (preference != null){
+                preference.setLayoutResource(LAYOUTS_N[i]);
             }
-            final Drawable icon = preference.getIcon();
-            if (icon != null) {
-                icon.setTint(tintColor);
+        }
+        for (int i = 0; i < CHANGE_LAYOUT_AND_ORDER_KEYS.length; i++) {
+            Preference preference = findPreference(CHANGE_LAYOUT_AND_ORDER_KEYS[i]);
+            if (preference != null){
+                preference.setLayoutResource(R.layout.rdnt_card_device);
+                preference.setOrder(12);
             }
         }
     }
